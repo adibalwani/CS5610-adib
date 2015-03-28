bfa4b14a755e2ce12b2085f992b180ea3463badd
adibalwani <adib@ccs.neu.edu>
2015-03-27 20:52:29 -0400

    Search result display

--- v4.0/project/js/index.js
+++ v4.0/project/js/index.js
@@ -25,15 +25,49 @@ app.controller("HomeController", function ($scope, $location, $http) {
                         var dest_latitude = results[0].geometry.location.lat();
                         var dest_longitude = results[0].geometry.location.lng();
 
-                        $http.get("https://api.car.ma:443/v2/trips/search?client_id=ext-adib-alwani&originLon=" + origin_longitude + "&originLat=" + origin_latitude + "&destinationLon=" + dest_longitude + "&destinationLat=" + dest_latitude + "&&&tripType=RIDE_OR_DRIVE&departureTimeStart=" + moment(new Date($scope.date).getTime()).unix() + "&departureTimeEnd=-1&onlineSince=-1&originRadius=10000.0&destinationRadius=10000.0&searchBoxPaddingDistance=10000.0&&adherence=1.0&sortBy=START_TIME_ORIGIN_DISTANCE&pageNum=1&pageSize=20&tripFields=LOCATIONS%2CLOCATION_ADDRESSES%2CDISTANCE%2CSCHEDULE&userFields=FULL_PUBLIC")
+                        $http.get("https://api.car.ma:443/v2/trips/search?client_id=ext-adib-alwani&originLon=" + origin_longitude + "&originLat=" + origin_latitude + "&destinationLon=" + dest_longitude + "&destinationLat=" + dest_latitude + "&&&tripType=RIDE_OR_DRIVE&departureTimeStart=" + moment(new Date($scope.date).getTime()).unix() + "&departureTimeEnd=-1&onlineSince=-1&originRadius=10000.0&destinationRadius=10000.0&searchBoxPaddingDistance=10000.0&&adherence=1.0&sortBy=START_TIME_ORIGIN_DISTANCE&pageNum=1&pageSize=20&tripFields=LOCATIONS%2CLOCATION_ADDRESSES%2CDISTANCE%2CSCHEDULE%2CESTIMATED_EARNINGCOST%2CUSER_ROLE&userFields=FULL_PUBLIC")
                         .success(function (response) {
                             console.log(response);
+                            for (var i in response.trips) {
+                                /*Convert startMinutes to Hours and Minutes of Local Time*/
+                                var hours = Math.floor(response.trips[i].schedule.startMinutes / 60);
+                                var minutes = response.trips[i].schedule.startMinutes % 60;
+                                if (hours > 12) {
+                                    var ampm = "PM";
+                                    hours = hours - 12;
+                                } else {
+                                    var ampm = "AM";
+                                }
+                                response.trips[i].schedule.hours = appendZero(hours);
+                                response.trips[i].schedule.minutes = appendZero(minutes);
+                                response.trips[i].schedule.ampm = ampm;
+
+                                /*Change user role*/
+                                if (response.trips[i].userRole == "RIDE") {
+                                    response.trips[i].userRole = "Rider";
+                                } else {
+                                    response.trips[i].userRole = "Driver";
+                                }
+                                
+
+                                /*Convert metres to miles*/
+                                response.trips[i].distance = Math.round(getMiles(response.trips[i].distance) * 10) / 10;
+                            }
+                            $scope.searchResults = response.trips;
                         });
                     }
                 });
             }
         });
     };
+
+    /*Append zero to time*/
+    function appendZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
 
     /*Add URL for login*/
     var index_page = "http://localhost:61854/project/index.html";
